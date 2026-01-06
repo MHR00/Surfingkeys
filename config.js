56038c3a1a271ca4fcbdd3a17944213e755c9d85
m.rostampour <m.rostampour@daya.com>
2026-01-06 13:53:37 +0330

    ad

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ api.mapkey('<ctrl-y>', 'Show me the money', function() {
     api.Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
 });
 
+//Test
 // an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
 api.map('gt', 'T');
 
